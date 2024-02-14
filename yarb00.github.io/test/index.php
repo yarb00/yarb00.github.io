940376ec097d51c5516b18c113dfd89063b488b5
yarb00 <yaroslav.biba@outlook.com>
2024-02-14 20:13:46 +0800

    Create index.php
    Testing php

---
+++ yarb00.github.io/test/index.php
@@ -0,0 +1,44 @@
+<!doctype html>
+<html lang="ru-RU">
+  	<head>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<link rel="stylesheet" href="main.css" type="text/css" charset="utf-8">
+
+		<title>yarb00</title>
+		<meta name="description" content="Все проекты от yarb00.">
+		<meta name="keywords" content="yarb00, github, yarb, ярб, йарб, ярб00, йарб00, гитхаб, сайт" />
+
+		<!—for google search console—>
+		<meta name="google-site-verification" content="EkfNiM2onpys1Ev3iumUqZMNh48BbS8vIDi2FsCgp1o" />
+  	</head>
+  	<body>
+		<header>
+			<div id="logo">
+				<a href="" title="На главную">
+					<img src="images/yarb00.jpg" title="yarb00.github.io" alt="yarb00.github.io">
+					<span>yarb00</span>
+				</a>
+			</div>
+			<div id="about">
+				<a href="projects/" title="Все проекты">Проекты</a>
+			</div>
+		</header>
+
+		<h1>   test</h1>
+
+		<h2>This site is in Russian, and there is no English translation yet!</h2>
+
+		<h3>   </h3>
+
+		<h1>Добро пожаловать!</h1>
+
+		<h5>   </h5>
+
+		Я - <a href="https://github.com/yarb00">yarb00</a>. Я - C++ разработчик и иногда пишу различные прикольные програмки на C++, и выкладываю их сюда (на GitHub).
+
+		<h6>   </h6>
+
+		Если вам стало интересно, то можете ознакомиться с моими проектами нажав кнопку "Проекты" вверху или перейдя по <a href="projects/">ссылке</a>.
+	</body>
+</html>
